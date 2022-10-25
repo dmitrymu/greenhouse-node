4e3a4e8109fbbd4ecd3949dc069a91f7bc88fefb
Dmitry M <d.v.mukhortov@gmail.com>
2022-10-24 22:58:25 -0700

    Separate console from main()

--- src/node/main/console.c
+++ src/node/main/console.c
@@ -1,153 +1,23 @@
-#
-
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <unistd.h>
-#include "esp_log.h"
+#include "console.h"
+#include "cmd_sys.h"
 #include "esp_console.h"
-#include "esp_spi_flash.h"
-#include "esp_system.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 
-static void register_free(void);
-static void register_heap(void);
-static void register_version(void);
-static void register_restart(void);
-static void register_tasks(void);
+static const int CONSOLE_MAX_COMMAND_LINE_LENGTH = 256;
 
-static const char *TAG = "cmd_system";
-
-void register_system_common()
+void console_run()
 {
-    register_free();
-    register_heap();
-    register_version();
-    register_restart();
-    register_tasks();
-}
+  esp_console_repl_t *repl = NULL;
+  esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
+  /* Prompt to be printed before each line.
+   * This can be customized, made dynamic, etc.
+   */
+  repl_config.prompt = ">";
+  repl_config.max_cmdline_length = CONSOLE_MAX_COMMAND_LINE_LENGTH;
 
-void register_system(void)
-{
-    register_system_common();
-}
+  esp_console_register_help_command();
+  register_system();
 
-
-/* 'version' command */
-static int get_version(int argc, char **argv)
-{
-    esp_chip_info_t info;
-    esp_chip_info(&info);
-    printf("IDF Version:%s\r\n", esp_get_idf_version());
-    printf("Chip info:\r\n");
-    printf("\tmodel:%s\r\n", info.model == CHIP_ESP32 ? "ESP32" : "Unknown");
-    printf("\tcores:%d\r\n", info.cores);
-    printf("\tfeature:%s%s%s%s%d%s\r\n",
-           info.features & CHIP_FEATURE_WIFI_BGN ? "/802.11bgn" : "",
-           info.features & CHIP_FEATURE_BLE ? "/BLE" : "",
-           info.features & CHIP_FEATURE_BT ? "/BT" : "",
-           info.features & CHIP_FEATURE_EMB_FLASH ? "/Embedded-Flash:" : "/External-Flash:",
-           spi_flash_get_chip_size() / (1024 * 1024), " MB");
-    printf("\trevision number:%d\r\n", info.revision);
-    return 0;
-}
-
-static void register_version(void)
-{
-    const esp_console_cmd_t cmd = {
-        .command = "version",
-        .help = "Get version of chip and SDK",
-        .hint = NULL,
-        .func = &get_version,
-    };
-    ESP_ERROR_CHECK( esp_console_cmd_register(&cmd) );
-}
-
-/** 'restart' command restarts the program */
-
-static int restart(int argc, char **argv)
-{
-    ESP_LOGI(TAG, "Restarting");
-    esp_restart();
-}
-
-static void register_restart(void)
-{
-    const esp_console_cmd_t cmd = {
-        .command = "restart",
-        .help = "Software reset of the chip",
-        .hint = NULL,
-        .func = &restart,
-    };
-    ESP_ERROR_CHECK( esp_console_cmd_register(&cmd) );
-}
-
-/** 'free' command prints available heap memory */
-
-static int free_mem(int argc, char **argv)
-{
-    printf("%d\n", esp_get_free_heap_size());
-    return 0;
-}
-
-static void register_free(void)
-{
-    const esp_console_cmd_t cmd = {
-        .command = "free",
-        .help = "Get the current size of free heap memory",
-        .hint = NULL,
-        .func = &free_mem,
-    };
-    ESP_ERROR_CHECK( esp_console_cmd_register(&cmd) );
-}
-
-/* 'heap' command prints minumum heap size */
-static int heap_size(int argc, char **argv)
-{
-    uint32_t heap_size = heap_caps_get_minimum_free_size(MALLOC_CAP_DEFAULT);
-    printf("min heap size: %u\n", heap_size);
-    return 0;
-}
-
-static void register_heap(void)
-{
-    const esp_console_cmd_t heap_cmd = {
-        .command = "heap",
-        .help = "Get minimum size of free heap memory that was available during program execution",
-        .hint = NULL,
-        .func = &heap_size,
-    };
-    ESP_ERROR_CHECK( esp_console_cmd_register(&heap_cmd) );
-
-}
-
-/** 'tasks' command prints the list of tasks and related information */
-
-static int tasks_info(int argc, char **argv)
-{
-    const size_t bytes_per_task = 40; /* see vTaskList description */
-    char *task_list_buffer = malloc(uxTaskGetNumberOfTasks() * bytes_per_task);
-    if (task_list_buffer == NULL) {
-        ESP_LOGE(TAG, "failed to allocate buffer for vTaskList output");
-        return 1;
-    }
-    fputs("Task Name\tStatus\tPrio\tHWM\tTask#", stdout);
-    fputs("\tAffinity", stdout);
-    fputs("\n", stdout);
-    vTaskList(task_list_buffer);
-    fputs(task_list_buffer, stdout);
-    free(task_list_buffer);
-    return 0;
-}
-
-static void register_tasks(void)
-{
-    const esp_console_cmd_t cmd = {
-        .command = "tasks",
-        .help = "Get information about running tasks",
-        .hint = NULL,
-        .func = &tasks_info,
-    };
-    ESP_ERROR_CHECK( esp_console_cmd_register(&cmd) );
+  esp_console_dev_uart_config_t hw_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_console_new_repl_uart(&hw_config, &repl_config, &repl));
+  ESP_ERROR_CHECK(esp_console_start_repl(repl));
 }
--- src/node/main/main.c
+++ src/node/main/main.c
@@ -1,12 +1,8 @@
 #include "console.h"
-#include "esp_console.h"
 #include "esp_log.h"
-#include "esp_system.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "nvs_flash.h"
-
-static const int CONSOLE_MAX_COMMAND_LINE_LENGTH = 256;
 
 static void initialize_nvs(void)
 {
@@ -23,22 +19,7 @@ void app_main(void)
 {
   static const char *tag = "hello";
 
-  esp_console_repl_t *repl = NULL;
-  esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
-  /* Prompt to be printed before each line.
-   * This can be customized, made dynamic, etc.
-   */
-  repl_config.prompt = ">";
-  repl_config.max_cmdline_length = CONSOLE_MAX_COMMAND_LINE_LENGTH;
-
   initialize_nvs();
-
-  esp_console_register_help_command();
-  register_system();
-
-  esp_console_dev_uart_config_t hw_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
-  ESP_ERROR_CHECK(esp_console_new_repl_uart(&hw_config, &repl_config, &repl));
-  ESP_ERROR_CHECK(esp_console_start_repl(repl));
 
   // Should never be executed.
   ESP_LOGI(tag, "Restarting now.");
